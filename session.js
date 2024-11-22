f03dbaeb8684ca707ac7b7375ab39619c0dda0cc
Asizsj <samarasinghajayakodi365@gmail.com>
2024-11-22 22:58:09 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=SQ0iVYiK#qpziDDQROimwMsxiE0s3vgOp_meGhE1RAQRwSQoaqJU",
 };
